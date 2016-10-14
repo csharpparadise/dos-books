3c61a35ab078aad81e9a4e5ff84edc428a2cca3a
Andreas Gräfe <andreasgraefe@gmail.com>
2016-10-14 11:27:09 +0200

    book component added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Book } from '../shared/book';
 @Component({
   selector: 'devos-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  books: string[];
+  books: Array<Book> = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.books = ['Typescript', 'Angualr 2'];
+    this.books.push(new Book('987-123456789', 'Angular 2', 'Super Buch', 10));
+    this.books.push(new Book('987-123456788', 'Angular 1.4', 'Super Buch vom Vorgänger', 10));
+    this.books.push(new Book('987-123456787', 'Typescript', 'alles wird besser!', 10));
   }
 }
---
+++ src/app/shared/book.ts
@@ -0,0 +1,8 @@
+export class Book {
+    constructor(
+        public isbn: string,
+        public title: string,
+        public description: string,
+        public rating = 0
+    ) {}
+}
