533f30e566bfdc0b7949d784224be867e24729ab
Andreas Gräfe <andreasgraefe@gmail.com>
2016-10-14 11:16:38 +0200

    Books as string array added and shown in view

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  books: string[];
+
   constructor() { }
 
   ngOnInit() {
+    this.books = ['Typescript', 'Angualr 2'];
   }
-
 }
